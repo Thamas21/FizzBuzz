e092aac10169398849c37309b03bd52e48f7b36c
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2021-04-21 14:35:32 -0700

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,5 +5,20 @@ def fizzbuzz(number):
             print(f"{number} is a Fizz")
         elif number % 5 == 0:
             print(f"{number} is a Buzz")
+        else: 
+            print(number)
 
-fizzbuzz(50)
+fizzbuzz(60)
+def fizzybuzzy():
+    counter = 1
+    while counter < 101:
+        if counter % 3 == 0 and counter % 5 == 0:
+            print(f"{counter} is a FizzBuzz")
+        elif counter % 3 == 0:
+            print(f"{counter} is a Fizz")
+        elif counter % 5 == 0:
+            print(f"{counter} is a Buzz")
+        else: 
+            print(counter)
+        counter += 1
+fizzybuzzy()
