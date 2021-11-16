b18e7b86caf0651bdd7919eeae6909cc228ce4c1
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2021-11-16 15:28:19 -0800

    Added fizzbuzz class

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
 def fizzbuzz(number):
-        if number % 3 == 0 and number % 5 == 0:
-            print(f"{number} is a FizzBuzz")
-        elif number % 3 == 0:
-            print(f"{number} is a Fizz")
-        elif number % 5 == 0:
-            print(f"{number} is a Buzz")
-        else: 
-            print(number)
+    if number % 3 == 0 and number % 5 == 0:
+        print(f"{number} is a FizzBuzz")
+    elif number % 3 == 0:
+        print(f"{number} is a Fizz")
+    elif number % 5 == 0:
+        print(f"{number} is a Buzz")
+    else:
+        print(number)
+
 
 fizzbuzz(60)
+
 
 def fizzybuzzy():
     counter = 1
@@ -19,7 +21,29 @@ def fizzybuzzy():
             print(f"{counter} is a Fizz")
         elif counter % 5 == 0:
             print(f"{counter} is a Buzz")
-        else: 
+        else:
             print(counter)
         counter += 1
+
+
 fizzybuzzy()
+
+
+class FizzBuzz:
+
+    def __init__(self, number):
+        self.number = number
+
+    def game(self):
+        if self.number % 3 == 0 and self.number % 5 == 0:
+            print(f"{self.number} is a FizzBuzz")
+        elif self.number % 3 == 0:
+            print(f"{self.number} is a Fizz")
+        elif self.number % 5 == 0:
+            print(f"{self.number} is a Buzz")
+        else:
+            print(self.number)
+
+
+fizz_buzz = FizzBuzz(47)
+fizz_buzz.game()
